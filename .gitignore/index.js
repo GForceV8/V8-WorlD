613ca36b24ec4881cc594250fb9c27a735550bd8
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2018-09-30 04:02:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("| MultiGaming |");
+    bot.user.setGame("| V8-WorlD |");
     console.log("Le bot a bien ete connecte")
 });
 
