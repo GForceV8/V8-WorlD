83f75779213336425792a4eca797d4933c9d7342
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2018-10-04 11:15:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("|À la chasse au YoYo|");
+    bot.user.setGame("|À la chasse au YoYo :p|");
     console.log("Le bot a bien ete connecte")
 });
 
